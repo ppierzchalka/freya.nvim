b70e5ac724d6ea072d2ebf3cffce309842b21d77
Przemysław Pierzchałka <przemekpierzchalka@gmail.com>
2025-07-19 13:48:23 +0200

    add transparency

--- lua/custom/plugins/transparency.lua
+++ lua/custom/plugins/transparency.lua
@@ -1,49 +1,22 @@
--- Transparency support
+-- Transparency support (using manual highlight setting in plugin/after/transparency.lua)
+-- This plugin is kept minimal for the toggle functionality only
 return {
   "xiyaowong/transparent.nvim",
   lazy = false,
   opts = {
-    groups = { -- table: default groups
-      "Normal",
-      "NormalNC",
-      "Comment",
-      "Constant",
-      "Special",
-      "Identifier",
-      "Statement",
-      "PreProc",
-      "Type",
-      "Underlined",
-      "Todo",
-      "String",
-      "Function",
-      "Conditional",
-      "Repeat",
-      "Operator",
-      "Structure",
-      "LineNr",
-      "Tree",
-      "NonText",
-      "SignColumn",
-      "CursorLineNr",
-      "EndOfBuffer",
-    },
-    extra_groups = { -- table: additional groups that should be cleared
-      "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-      "NvimTreeNormal", -- NvimTree
-      "NeoTreeNormal", -- NeoTree
-      "NeoTreeNormalNC",
-    },
-    exclude_groups = {}, -- table: groups you don't want to clear
+    enable = false, -- Don't auto-enable, we handle it manually
+    groups = {},
+    extra_groups = {},
+    exclude_groups = {},
   },
   config = function(_, opts)
     require("transparent").setup(opts)
     
     -- Keymaps for toggling transparency
-    vim.keymap.set("n", "<leader>tt", "<cmd>TransparentToggle<CR>", { desc = "Toggle Transparency" })
-    
-    -- Auto-enable transparency (optional)
-    -- require("transparent").clear_prefix("BufferLine")
-    -- require("transparent").clear_prefix("NvimTree")
+    vim.keymap.set("n", "<leader>tt", function()
+      require("transparent").toggle()
+      -- Reapply our manual transparency after toggle
+      dofile(vim.fn.stdpath("config") .. "/plugin/after/transparency.lua")
+    end, { desc = "Toggle Transparency" })
   end,
 }
